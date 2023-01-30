f1565146c397acdc918b862a983425932947012a
cbasile <basilechiara@libero.it>
2023-01-30 22:38:04 +0100

    new base class | Psi decay

--- PRELIMINARY/src/CheckGenLevPsi2S.C
+++ PRELIMINARY/src/CheckGenLevPsi2S.C
@@ -3,6 +3,13 @@
 #include <TH2.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+	
+
+CheckGenLevPsi2S::CheckGenLevPsi2S(TTree *tree, const TString& tags ) : MCbase_B0toPsi2SK0s(tree, tags){
+
+	outFilePath_ = "./outRoot/CheckGenLev_" + tags_ + ".root"; 
+
+}//CheckGenLevPsi2S()
 
 void CheckGenLevPsi2S::Loop()
 {
@@ -19,9 +26,9 @@ void CheckGenLevPsi2S::Loop()
 
    fChain->SetBranchStatus("HLT_DoubleMu4_JpsiTrk_Displaced",1);
    fChain->SetBranchStatus("HLT_Dimuon25_Jpsi",1);
-   fChain->SetBranchStatus("HLT_DoubleMu4_JpsiTrkTrk_Displaced",1);
-   fChain->SetBranchStatus("HLT_Dimuon18_PsiPrime",1);
-   fChain->SetBranchStatus("HLT_DoubleMu4_PsiPrimeTrk_Displaced",1);
+  // fChain->SetBranchStatus("HLT_DoubleMu4_JpsiTrkTrk_Displaced",1);
+  // fChain->SetBranchStatus("HLT_Dimuon18_PsiPrime",1);
+  // fChain->SetBranchStatus("HLT_DoubleMu4_PsiPrimeTrk_Displaced",1);
    fChain->SetBranchStatus("nGenPart",1);
    fChain->SetBranchStatus("GenPart_pdgId",1);
    fChain->SetBranchStatus("GenPart_genPartIdxMother",1);
@@ -102,7 +109,7 @@ void CheckGenLevPsi2S::Loop()
 			h_GptSL_mu.Fill(GenP4_Mum.Pt());
 		}
     }
-    TFile gen_outfile("./outRoot/CheckGenLev_" + tags_ + ".root","RECREATE"); // create the output file
+    TFile gen_outfile(outFilePath_,"RECREATE"); // create the output file
     std::cout << " ---> [OUT] output saved in file " << gen_outfile.GetName() << std::endl;
     h_Gpt_mu.Write();
     h_GptL_mu.Write();
@@ -143,87 +150,3 @@ void CheckGenLevPsi2S::GenPart_FillKinHist(ROOT::Math::PtEtaPhiMVector* GenVec, 
 	h_mass->Fill(GenVec->M());
 
 }//GenPart_FillKinHist()
-
-void CheckGenLevPsi2S::GenPartFillP4(){
-
-	Int_t MumIdx = -1, MupIdx = -1, PimIdx = -1 , PipIdx = -1, JPsiIdx = -1, Psi2SIdx = -1, K0sIdx = -1, B0Idx = -1;
-   for (UInt_t g = 0; g < nGenPart; g++){
-		// MUONS
-		if( (abs(GenPart_pdgId[g]) ==   isMum) &&
-			 (GenPart_pdgId[GenPart_genPartIdxMother[g]] == isJPsi) && 
-			 (GenPart_pdgId[GenPart_genPartIdxMother[GenPart_genPartIdxMother[g]]] == isPsi2S) &&
-			 (abs(GenPart_pdgId[GenPart_genPartIdxMother[GenPart_genPartIdxMother[GenPart_genPartIdxMother[g]]]]) == isB0) )
-			{
-			JPsiIdx = GenPart_genPartIdxMother[g];
-			Psi2SIdx = GenPart_genPartIdxMother[GenPart_genPartIdxMother[g]];
-			B0Idx = GenPart_genPartIdxMother[GenPart_genPartIdxMother[GenPart_genPartIdxMother[g]]];
-			if (GenPart_pdgId[g] ==   isMum) MumIdx = g;
-			if (GenPart_pdgId[g] ==  -isMum) MupIdx = g;
-			}
-		// PIONS
-      if( (abs(GenPart_pdgId[g]) ==   isPip) && 
-			 (GenPart_pdgId[GenPart_genPartIdxMother[g]] == isPsi2S) &&
-			 (abs(GenPart_pdgId[GenPart_genPartIdxMother[GenPart_genPartIdxMother[g]]]) == isB0) )
-			{
-			if (GenPart_pdgId[g] == -isPip) PimIdx = g;
-			if (GenPart_pdgId[g] ==  isPip) PipIdx = g;
-			}
-      if( (GenPart_pdgId[g] ==  isK0s) &&  (abs(GenPart_pdgId[GenPart_genPartIdxMother[g]]) == abs(isB0))) K0sIdx = g;
-
-
-   }//on generated ptl
-
-	if(0){
-		std::cout << " IDX Mu-  / Mu+  " << MumIdx  << "\t" << MupIdx << std::endl; 
-		std::cout << " IDX Pi-  / Pi+  " << PimIdx  << "\t" << PipIdx << std::endl; 
-		std::cout << " IDX JPsi " << JPsiIdx << std::endl; 
-		std::cout << " IDX Psi2S/ K0s  " << Psi2SIdx << "\t" << K0sIdx << std::endl; 
-		std::cout << " IDX B0  " << B0Idx << std::endl; 
-	}
-	
-	if(MumIdx > 0){   
-      GenP4_Mum.SetPt(GenPart_pt[MumIdx]);   GenP4_Mum.SetEta(GenPart_eta[MumIdx]);   
-		GenP4_Mum.SetPhi(GenPart_phi[MumIdx]); GenP4_Mum.SetM(mMuon);
-	} else std::cout << "No generated mu- is found" << std::endl;  
-
-	if(MupIdx > 0){   
-      GenP4_Mup.SetPt(GenPart_pt[MupIdx]);   GenP4_Mup.SetEta(GenPart_eta[MupIdx]);
-		GenP4_Mup.SetPhi(GenPart_phi[MupIdx]); GenP4_Mup.SetM(mMuon); 
-	} else std::cout << "No generated mu+ is found" << std::endl;  
-
-	if(JPsiIdx > 0){   
-      GenP4_JPsi.SetPt(GenPart_pt[JPsiIdx]);    GenP4_JPsi.SetEta(GenPart_eta[JPsiIdx]);
-		GenP4_JPsi.SetPhi(GenPart_phi[JPsiIdx]);  GenP4_JPsi.SetM(GenPart_mass[JPsiIdx]); 
-	} else std::cout << "No generated JPsi is found" << std::endl;  
-
-	if(PimIdx > 0){   
-      GenP4_Pim.SetPt(GenPart_pt[PimIdx]);   GenP4_Pim.SetEta(GenPart_eta[PimIdx]);
-		GenP4_Pim.SetPhi(GenPart_phi[PimIdx]); GenP4_Pim.SetM(mPion);
-	} else std::cout << "No generated pi- is found" << std::endl;  
-
-	if(PipIdx > 0){   
-      GenP4_Pip.SetPt(GenPart_pt[PipIdx]);   GenP4_Pip.SetEta(GenPart_eta[PipIdx]);
-		GenP4_Pip.SetPhi(GenPart_phi[PipIdx]); GenP4_Pip.SetM(mPion); 
-	} else std::cout << "No generated pi+ is found" << std::endl;  
-
-	if((PipIdx > 0) && (PimIdx > 0)){   
-      GenP4_PiPi = GenP4_Pip + GenP4_Pim;
-	} else std::cout << "No generated PiPi is found" << std::endl;  
-	
-	if(Psi2SIdx > 0){   
-      GenP4_Psi2S.SetPt(GenPart_pt[Psi2SIdx]);   GenP4_Psi2S.SetEta(GenPart_eta[Psi2SIdx]);
-		GenP4_Psi2S.SetPhi(GenPart_phi[Psi2SIdx]); GenP4_Psi2S.SetM(GenPart_mass[Psi2SIdx]); 
-	} else std::cout << "No generated X(3872) is found" << std::endl;  
-
-	if(K0sIdx > 0){   
-      GenP4_K0s.SetPt(GenPart_pt[K0sIdx]);   GenP4_K0s.SetEta(GenPart_eta[K0sIdx]);
-		GenP4_K0s.SetPhi(GenPart_phi[K0sIdx]); GenP4_K0s.SetM(GenPart_mass[K0sIdx]); 
-	} else std::cout << "No generated K0s is found" << std::endl;  
-
-	if(B0Idx >= 0){   
-      GenP4_B0.SetPt(GenPart_pt[B0Idx]);   GenP4_B0.SetEta(GenPart_eta[B0Idx]);
-		GenP4_B0.SetPhi(GenPart_phi[B0Idx]); GenP4_B0.SetM(GenPart_mass[B0Idx]); 
-	} else std::cout << "No generated B0 is found" << std::endl;  
-
-	
-}//GenPartFillP4()
